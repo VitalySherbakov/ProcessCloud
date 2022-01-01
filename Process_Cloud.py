a0556a7229914b8a494900fb0da09f30faa2ec28
Gendalf_Istari_2016 <palantir2018see@gmail.com>
2022-01-01 18:42:23 +0200

    dfgdfgfdg

--- Process_Cloud.py
+++ Process_Cloud.py
@@ -126,7 +126,7 @@ class WIFI_Cloud:
 						selectfilespath=f"{pathdir}/{files[0]}"
 					else:
 						print(f"Нету Сконвентированого файла {url_convert} из формата *.cap в *.hc22000")
-						quit()
+						sys.exit()
 			print(f"Файл Перебора: {selectfilespath}")
 			print(f"-----------------Выбор Метода 1-Полный 2-Тройной 3-Кусками-----------------")
 			etap_select=input("Выбор Метода: ")
