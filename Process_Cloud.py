216a829a1fa6a3c3d86ab1abe7e7be3b1cb231c7
Gendalf_Istari_2016 <palantir2018see@gmail.com>
2022-01-02 23:14:42 +0200

    3424

--- Process_Cloud.py
+++ Process_Cloud.py
@@ -221,6 +221,8 @@ class WIFI_Cloud:
 					print(f"Скорость-{dir_hc.Speed}")
 					print(f"Папка Сеть WIFI: {dir_wifi}")
 					print(f"Файл WIFI: {selectfilespath}")
+					for i3,li3 in enumerate(dir_hc.DictsProces):
+						print(f"Номер {li3+1} | Этап {li3}")
 					pod_etap_select2=input(f"Выбрать 1-{len(dir_hc.DictsProces)} Этапов: ")
 					index_pod_etap_select2=int(pod_etap_select2)
 					if (index_pod_etap_select2<len(dir_hc.DictsProces)):
